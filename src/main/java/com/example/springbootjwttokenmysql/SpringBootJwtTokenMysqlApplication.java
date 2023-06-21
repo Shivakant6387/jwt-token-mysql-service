3c51900bb6944b6f81ac1c4baa62bca265b2fdf3
Shivakant6387 <shivakant5875@gmail.com>
2023-06-21 23:06:26 +0530

    nothing to commit here

---
+++ src/main/java/com/example/springbootjwttokenmysql/SpringBootJwtTokenMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjwttokenmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtTokenMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtTokenMysqlApplication.class, args);
+	}
+
+}
